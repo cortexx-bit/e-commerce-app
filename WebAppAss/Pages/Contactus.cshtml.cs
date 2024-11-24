e0d7a98601dcd533ea9e9a8c521316caa4347d15
Игорь Вартик < @DESKTOP-AT5LO68>
2024-11-24 14:25:52 +0000

    Add project files.

---
+++ WebAppAss/Pages/Contactus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppAss.Pages
+{
+    public class ContactusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebAppAss/Pages/Hours&location.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppAss.Pages
+{
+    public class Hours_locationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
