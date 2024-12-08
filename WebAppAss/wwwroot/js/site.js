523f3d6c41051e48e93e4c12a70619856932dbf1
cortexx-bit <socol2004@mail.ru>
2024-12-08 21:55:28 +0000

    Implemented privacy and contact us pages. Changed the theme of the website to dark.

--- WebAppAss/wwwroot/js/site.js
+++ WebAppAss/wwwroot/js/site.js
@@ -1,15 +1,11 @@
 ﻿// site.js
 
 document.addEventListener('DOMContentLoaded', function () {
-    var logo = document.getElementById('logo');
+    const contactForm = document.getElementById('contactForm');
+    const successMessage = document.getElementById('successMessage');
 
-    document.addEventListener('scroll', function () {
-        if (window.scrollY > 0) {
-            logo.classList.add('scrolled');
-            logo.querySelector('img').src = '/images/logo-scrolled.png'; // Change to the scrolled logo
-        } else {
-            logo.classList.remove('scrolled');
-            logo.querySelector('img').src = '/images/logo.png'; // Change back to the original logo
-        }
+    contactForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        successMessage.style.display = 'block';
     });
 });
